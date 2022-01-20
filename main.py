d7a415ffab7bd9f9ca056573abd51eda34363e49
Danny W <32901175+wangodanny@users.noreply.github.com>
2022-01-20 20:38:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Requirements:
+#Show 5 sorting algorithms
+# Quick, Selection, Bubble, Insertion, Merge
+# Explain step by step
+# Enable user to pick the algorithm
+# Enable user input.
+
+print ("Hello and welcome to Sorting Explained, where we show step by step how the sorting algorithm you choose, works!\n")
+
+print("To start please choose the algorithm you want to use")
